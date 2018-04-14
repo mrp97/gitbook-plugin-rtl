b78fece0ac4eefdb23f482855f713ce2e3c0ce64
Mohammad <mr.rafiepour@gmail.com>
2018-04-14 17:40:52 +0430

    Change cheerio selector to find element in page content

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 var cheerio = require('cheerio');
 
-function makeRTL(text){
-  var elements = ['h1','h2','h3','h4','h5','h6','p'];
-  var $$ = cheerio.load(text);
-
-  elements.forEach(function(element){
-    $$('section').find(element).each(function(i, elem){
-      if(checkPersian($$(this).text().trimLeft().charAt(0))){
-        $$(this).attr('style','text-align:right;')
-      } else {
-        $$(this).attr('style','text-align:left;')
-      }
-    })
-  });
-
-  return $$.html();
-}
-
 function checkPersian(str){
   var p = /^[\u0600-\u06FF\s]+$/;
    if ( p.test(str) ) {
@@ -26,16 +9,14 @@ function checkPersian(str){
    }
 }
 
-
 module.exports = {
     // Map of hooks
     hooks: {
       page: function(page) {
         var elements = ['h1','h2','h3','h4','h5','h6','p'];
         var $ = cheerio.load(page.content);
-        console.log(page.content);
         elements.forEach(function(element){
-          $('section').find(element).each(function(i, elem){
+          $(element).each(function(i, elem){
             console.log(elem);
             const $this = $(this);
             if(checkPersian($this.text().trimLeft().charAt(0))){
